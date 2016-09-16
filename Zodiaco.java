eb22cda6dc8da225416b70b3a3418a7e1b054d19
WeirdEddyLog95 <wickedscoutftw95@hotmail.com>
2016-09-16 12:45:20 -0700

    Add files via upload
    Se desarrolla atributos para el programa de zodiacos

---
+++ Zodiaco.java
@@ -0,0 +1,18 @@
+package Proceso;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.*;
+import javax.swing.*;
+
+public class Zodiaco {
+	
+	 public static void main(String[] args) {
+		 //Commit 1, En el proyecto desarrollamos los atributos
+		String Nombre;
+		String Apellidos;
+		int Edad;
+		int NoControl;
+		LocalDate FechaNacimiento = null;
+		String signoZodiacal;
+	 }
+}
